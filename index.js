59ef664a5a07359d92e264ebf9a031493f4f9005
Hamzasuliman404 <hamza.ahmedsuliman@outlook.com>
2025-12-25 21:31:54 +0300

    All card added

--- index.js
+++ index.js
@@ -10,6 +10,16 @@ convertBtnEl.addEventListener("click",function(){
                 <h2>Length (Meter/Feet)</h2>
                 <p>${inputFiledEl.value} meters = ${(Number(inputFiledEl.value)* 3.281).toFixed(3)} feet | ${inputFiledEl.value} feet = ${(Number(inputFiledEl.value)/ 3.281).toFixed(3)} meters</p>
             </div>
+
+            <div class="unit-card">
+                <h2>Volume (Liters/Gallons)</h2>
+                <p>${inputFiledEl.value} liters = ${(Number(inputFiledEl.value)* 0.264).toFixed(3)} Gallons | ${inputFiledEl.value} Gallons = ${(Number(inputFiledEl.value)/ 0.264).toFixed(3)} liters</p>
+            </div>
+
+            <div class="unit-card">
+                <h2>Mass (Kilograms/Pounds)</h2>
+                <p>${inputFiledEl.value} kilos = ${(Number(inputFiledEl.value)* 2.204).toFixed(3)} Pounds | ${inputFiledEl.value} Pounds = ${(Number(inputFiledEl.value)/ 2.204).toFixed(3)} kilos</p>
+            </div>
     `
 })
 
